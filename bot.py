b9426b6ea27e499f4b44010509022ce06fb29343
Frija7 <Pavelnavalihin89176@gmail.com>
2024-09-21 20:05:52 +0700

    bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from bot_logic import gen_pass
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send(print(gen_pass(10)))
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTI4NzAxNDUzNTMxOTc4NTUxMw.GrFQDF.Rs5tIkacMymYuVmGPRSEP5YNqWu1QMhXYrqU2g")
